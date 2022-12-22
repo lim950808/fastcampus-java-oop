85b39d0981fee8157edea47a85acd158b02bfe25
jaeeoklim <lim080895@gmail.com>
2022-12-23 04:14:42 +0900

    디펜던시 추가
    spring-boot-starter 관련 디펜던시 추가

---
+++ src/test/java/com/fastcampus/javaoop/FastcampusJavaOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.javaoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusJavaOopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
