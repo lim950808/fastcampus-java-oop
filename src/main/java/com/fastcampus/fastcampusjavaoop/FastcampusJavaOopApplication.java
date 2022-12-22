a54d940817110c9fba04b2468314c474667d08b4
jaeeoklim <lim080895@gmail.com>
2022-12-23 03:34:09 +0900

    Initialize java + gradle
    패스트캠퍼스 예제 자바 + gradle 프로젝트 셋업

---
+++ src/main/java/com/fastcampus/fastcampusjavaoop/FastcampusJavaOopApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fastcampusjavaoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusJavaOopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusJavaOopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcampus/fastcampusjavaoop/FastcampusJavaOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fastcampusjavaoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusJavaOopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
